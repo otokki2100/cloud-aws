b554a5ba4cc72b6adde913a5f294ae48e17fe773
admin-mgmt <admin-mgmt@admin-mgmts-MacBook-Air.local>
2023-07-02 13:04:58 +0900

    update

--- compute-ec2/proxy/terraform/res_ec2_ansible.tf
+++ compute-ec2/proxy/terraform/res_ec2_ansible.tf
@@ -12,19 +12,12 @@ all:
       ansible_ssh_common_args: "-o StrictHostKeyChecking=no"
 
     proxy_backend:
-      ansible_host: ${module.proxy_backend.public_ip}
+      ansible_host: ${module.proxy_backend.private_ip}
       ansible_connection: ssh
       ansible_user: ${var.proxy_backend.user}
       ansible_ssh_private_key_file: id_rsa
-      ansible_ssh_common_args: "-o StrictHostKeyChecking=no"
-  EOT
-}
-
-resource "local_file" "ansible_cfg" {
-  filename        = "./ansible.cfg"
-  file_permission = "0600"
-  content  = <<-EOT
-[ssh_connection]
-ssh_args = -o ProxyCommand='ssh -q -W %h:%p -i id_rsa ${var.proxy_backend.user}@${module.proxy_backend.public_ip}'
+      ansible_ssh_common_args:
+        -o StrictHostKeyChecking=no
+        -o ProxyCommand="ssh -W %h:%p -q -i id_rsa ${var.proxy_frontend.user}@${module.proxy_frontend.public_ip}"
   EOT
 }
