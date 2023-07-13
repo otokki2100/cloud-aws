7909f0d710aa77227a76e29cf67bef2e8e485a60
admin-mgmt <admin-mgmt@admin-mgmts-MacBook-Air.local>
2023-07-14 08:39:08 +0900

    update

--- compute-ec2/simple/terraform/res_archive.tf
+++ compute-ec2/simple/terraform/res_archive.tf
@@ -2,5 +2,5 @@ provider "archive" {}
 
 resource "archive_file" "example" {
   source = "https://example.com/remote_file.txt"
-  destination = "script/local_file.txt"
+  destination = "script/script.sh"
 }
