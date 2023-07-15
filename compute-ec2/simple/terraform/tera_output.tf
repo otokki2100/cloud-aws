ffb6897af2fc626d82009944c1cb3bbf9ace8202
admin-mgmt <admin-mgmt@admin-mgmts-MacBook-Air.local>
2023-07-15 10:43:30 +0900

    update

--- compute-ec2/simple/terraform/tera_output.tf
+++ compute-ec2/simple/terraform/tera_output.tf
@@ -2,10 +2,14 @@ output "vm_id" {
   value = module.ec2.id
 }
 
-output "vm_user" {
-  value = var.ec2.user
-}
-
 output "vm_ip" {
   value = module.ec2.public_ip
+}
+
+output "vm_ssh" {
+  value = "ssh ${var.ec2.user}@${module.ec2.public_ip} -o StrictHostKeyChecking=no -i id_rsa"
+}
+
+output "vm_user" {
+  value = var.ec2.user
 }
