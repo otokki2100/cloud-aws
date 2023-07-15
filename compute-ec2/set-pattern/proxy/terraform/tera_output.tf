03262ad8c1ec964c3c7abb03dd01a8df215312c3
admin-mgmt <admin-mgmt@admin-mgmts-MacBook-Air.local>
2023-07-15 16:24:40 +0900

    update

--- compute-ec2/set-pattern/proxy/terraform/tera_output.tf
+++ compute-ec2/set-pattern/proxy/terraform/tera_output.tf
@@ -1,29 +1,29 @@
-output "vm_proxy_frontend_id" {
+output "vm_proxy_a_frontend_id" {
   value = module.proxy_frontend.id
+}
+
+output "vm_proxy_a_frontend_ip" {
+  value = module.proxy_frontend.public_ip
+}
+
+output "vm_proxy_a_frontend_user" {
+  value = var.proxy_frontend.user
+}
+
+output "vm_proxy_a_frontend_z_ssh" {
+  value = "ssh ${var.proxy_frontend.user}@${module.proxy_frontend.public_ip} -o StrictHostKeyChecking=no -i id_rsa"
 }
 
 output "vm_proxy_backend_id" {
   value = module.proxy_backend.id
 }
 
-output "vm_proxy_frontend_ip" {
-  value = module.proxy_frontend.public_ip
-}
-
 output "vm_proxy_backend_ip" {
   value = module.proxy_backend.public_ip
 }
 
-output "vm_proxy_frontend_user" {
-  value = var.proxy_frontend.user
-}
-
 output "vm_proxy_backend_user" {
   value = var.proxy_backend.user
-}
-
-output "vm_proxy_frontend_z_ssh" {
-  value = "ssh ${var.proxy_frontend.user}@${module.proxy_frontend.public_ip} -o StrictHostKeyChecking=no -i id_rsa"
 }
 
 output "vm_proxy_backend_z_ssh" {
--- compute-ec2/set-pattern/simple-02-double/terraform/tera_output.tf
+++ compute-ec2/set-pattern/simple-02-double/terraform/tera_output.tf
@@ -2,28 +2,28 @@ output "vm_1_id" {
   value = module.ec2_1.id
 }
 
-output "vm_2_id" {
-  value = module.ec2_2.id
-}
-
 output "vm_1_ip" {
   value = module.ec2_1.public_ip
-}
-
-output "vm_2_ip" {
-  value = module.ec2_2.public_ip
 }
 
 output "vm_1_user" {
   value = var.ec2_1.user
 }
 
-output "vm_2_user" {
-  value = var.ec2_2.user
-}
-
 output "vm_1_z_ssh" {
   value = "ssh ${var.ec2_1.user}@${module.ec2_1.public_ip} -o StrictHostKeyChecking=no -i id_rsa"
+}
+
+output "vm_2_id" {
+  value = module.ec2_2.id
+}
+
+output "vm_2_ip" {
+  value = module.ec2_2.public_ip
+}
+
+output "vm_2_user" {
+  value = var.ec2_2.user
 }
 
 output "vm_2_z_ssh" {
